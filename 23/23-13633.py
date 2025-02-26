6fd25c8f89774229c07aff31c794a41adbb60270
osipm01 <osip2004@vk.com>
2025-02-26 10:52:22 +0300

    23

---
+++ 23/23-13633.py
@@ -0,0 +1,11 @@
+
+def f(x, y):
+    if x == y:
+        return 1
+    if x > y:
+        return 0
+    else:
+        return f(x + 1, y) + f(x * 2, y) + f(x * 3, y)
+    
+
+print(f(2, 15) * f(15 , 30))
